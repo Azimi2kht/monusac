c8e60868293b01a20e4c3da0297e6fc378dfc000
Azimi2kht <seyedhamidazimidokht@gmail.com>
2024-06-03 17:03:36 +0330

    add dataset file

---
+++ mmseg/datasets/monusac.py
@@ -0,0 +1,38 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import mmengine.fileio as fileio
+
+from mmseg.registry import DATASETS
+from .basesegdataset import BaseSegDataset
+
+
+@DATASETS.register_module()
+class MoNuSACDataset(BaseSegDataset):
+    """MoNuSAC dataset.
+
+    In segmentation map annotation for MoNuSAC, 0 stands for background, which is
+    included in 2 categories. ``reduce_zero_label`` is fixed to False. The
+    ``img_suffix`` is fixed to '.png' and ``seg_map_suffix`` is fixed to
+    '_manual1.png'.
+    """
+
+    METAINFO = dict(
+        classes=("background", "nucleus"),
+        palette=[[0, 0, 0], [255, 255, 255]],
+    )
+
+    def __init__(
+        self,
+        img_suffix=".tif",
+        seg_map_suffix="_manual1.png",
+        reduce_zero_label=False,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            img_suffix=img_suffix,
+            seg_map_suffix=seg_map_suffix,
+            reduce_zero_label=reduce_zero_label,
+            **kwargs,
+        )
+        assert fileio.exists(
+            self.data_prefix["img_path"], backend_args=self.backend_args
+        )
